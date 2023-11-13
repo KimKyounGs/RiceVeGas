81e407d311c81b08486e5f4a4fb7034cde53b514
seongjun531 <98207011+seongjun531@users.noreply.github.com>
2023-11-13 14:24:01 +0900

    GameStart시 초기화 까지

--- Assets/02.Scipts/GameStartUI/StartManager.cs
+++ Assets/02.Scipts/GameStartUI/StartManager.cs
@@ -7,20 +7,48 @@ using UnityEngine.SceneManagement;
 
 public class StartManager : MonoBehaviour
 {
+    public GameObject startPanel;
+    public GameObject GamePanel;
+    private Transform parentObject;
     void Start()
     {
-        Time.timeScale = 0f;
+        
     }
+
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.P))
         {
-            SceneManager.LoadScene("LobbyScene");
+            RestartGame();
+            startPanel.SetActive(true);
         }
     }
+
     public void ResumeTime()
     {
         Time.timeScale = 1.0f;
+    }   
+
+    void RestartGame()
+    {
+        DestroyAllGameObjects();
+
+        Instantiate(GamePanel, transform.position, Quaternion.identity, transform);
+    }
+
+    void DestroyAllGameObjects()
+    {
+        {
+            if (parentObject == null)
+            {
+                parentObject = transform;
+            }
+
+            foreach (Transform child in parentObject)
+            {
+                Destroy(child.gameObject);
+            }
+        }
     }
 }

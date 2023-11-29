f293cac2aba65fb5cda08753bf360ec4c3430442
seongjun531 <98207011+seongjun531@users.noreply.github.com>
2023-11-30 01:32:26 +0900

    다시

--- Assets/02.Scipts/GameStartUI/GameStartUIButton.cs
+++ Assets/02.Scipts/GameStartUI/GameStartUIButton.cs
@@ -6,17 +6,18 @@ using Photon.Pun;
 using Photon.Realtime;
 using Photon.Pun.UtilityScripts;
 
-public class GameStartUIButton : MonoBehaviourPunCallbacks
+public class GameStartUIButton : MonoBehaviourPun
 {
-    public PhotonView photonView;
+    public PhotonView PV;
     public GameObject Button;
     public GameObject startPanel;
+    public GameObject ReadyPanel;
     public GameObject InGamePanel;
     //public GameObject gamePlayObject;
 
     public void StartGameHandler()
     {
-        photonView = GetComponent<PhotonView>();
+        PV = GetComponent<PhotonView>();
         startPanel.SetActive(false);    //시작 패널 닫고
         InGamePanel.SetActive(true);    //인게임 패널 오픈
         //StartManager gamePlayScript = gamePlayObject.GetComponent<StartManager>();
@@ -31,16 +32,11 @@ public class GameStartUIButton : MonoBehaviourPunCallbacks
     }
 
     [PunRPC]
-    public void DisableReadyUI()
+    private void CloseReadyPanel()
     {
-        // 여기에서 다른 플레이어들의 레디 UI를 비활성화
-        // 예를 들어, 모든 플레이어의 레디 UI가 ReadyPanel이라 가정하면:
-        GameObject[] readyPanels = GameObject.FindGameObjectsWithTag("ReadyPanel");
-
-        foreach (GameObject panel in readyPanels)
-        {
-            panel.SetActive(false);
-        }
+        // "Ready" 패널을 닫음
+        ReadyPanel.SetActive(false);
+        InGamePanel.SetActive(true);
     }
 }
 
